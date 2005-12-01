fd8dba197a9c6b86bb5663d1ee196925f1f7459a
Timothée Foucart <timothee.foucart@apinc.org>
2005-12-01 00:17:58 +0000

    Import of TFOUCART/WWW-Sucksub-Frigo-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Sucksub-Frigo
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         TFOUCART/WWW-Sucksub-Frigo-0.02.tar.gz
    gitpan-cpan-author:       TFOUCART
    gitpan-cpan-maturity:     released

--- lib/WWW/Sucksub/Frigo.pm
+++ lib/WWW/Sucksub/Frigo.pm
@@ -5,11 +5,11 @@ WWW::Sucksub::Frigo - Automated access to frigorifix subtibles database
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
